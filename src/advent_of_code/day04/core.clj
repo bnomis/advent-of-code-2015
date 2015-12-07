c839c39286782fa4d1f54f105147cbafe5723482
Simon Blanchard <simonb@yajogo.com>
2015-12-07 21:27:08 +0800

    day 4 part 2

--- src/advent_of_code/day04/core.clj
+++ src/advent_of_code/day04/core.clj
@@ -6,29 +6,39 @@
     true
     false))
 
+(defn starts-with-six-zeros [number]
+  (if (= "000000" (apply str (take 6 (seq number))))
+    true
+    false))
+
 (defn make-hex [input]
-  ;;(println "make-hex" input)
   (apply str (map (partial format "%02x") input)))
 
 (defn make-md5 [input]
   (let [md (MessageDigest/getInstance "MD5")
         bytes (.getBytes input "UTF-8")]
-    ;;(println "make-md5: bytes" bytes)
     (.digest md bytes)))
 
 (defn make-hash [input]
   (let [h (make-hex (make-md5 input))]
-    ;;(println "make-hash:" input "->" h)
     h))
 
 (defn find-number [input]
   (loop [number 1]
     (let [joined (str input number)
           hashed (make-hash joined)]
-      ;;(println input number joined hashed)
       (if (starts-with-five-zeros hashed)
         number
         (recur (inc number))))))
 
+(defn find-number-6 [input]
+  (loop [number 1]
+    (let [joined (str input number)
+          hashed (make-hash joined)]
+      (if (starts-with-six-zeros hashed)
+        number
+        (recur (inc number))))))
+
 (defn run []
-  (println "Day 04:" (find-number "yzbqklnj")))
+  (println "Day 04, part 1:" (find-number "yzbqklnj"))
+  (println "Day 04, part 2:" (find-number-6 "yzbqklnj")))
