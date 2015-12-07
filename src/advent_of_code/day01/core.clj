57ef82b509718232f03a4a4ec48aa82bc4b8dd51
Simon Blanchard <simonb@yajogo.com>
2015-12-07 20:27:44 +0800

    day 1 part 2

--- src/advent_of_code/day01/core.clj
+++ src/advent_of_code/day01/core.clj
@@ -7,7 +7,24 @@
                           (dec acc))) 0 input)]
     count))
 
+(defn verticalise [floor action]
+  (if (= \( action)
+    (inc floor)
+    (dec floor)))
+
+(defn find-basement [input]
+  (loop [floor 0
+          pos 0
+          current (first input)
+          input (rest input)]
+    (if (= -1 floor)
+      pos
+      (do
+        (recur (verticalise floor current) (inc pos) (first input) (rest input))))))
+
 (defn run []
   (let [input (seq (slurp "src/advent_of_code/day01/input.txt"))
-        count (find-floor input)]
-    (println "Day 01:" count)))
+        count (find-floor input)
+        basement (find-basement input)]
+    (println "Day 01, part 1:" count)
+    (println "Day 01, part 2:" basement)))
