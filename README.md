7050007c22cf8e02b23d9391c18a027c0f8b8230
Simon Blanchard <simonb@yajogo.com>
2015-12-31 20:14:53 +0800

    update readme and project.clj

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
-# Advent of Code 2015
+# Advent of Code 2015 in Clojure
 
-http://adventofcode.com/
+Clojure solutions for the [2015 Advent of Code](http://adventofcode.com/)
 
-https://www.reddit.com/r/adventofcode
+There is an active [subreddit](https://www.reddit.com/r/adventofcode) where solutions are discussed.
 
-How To run
+## Running
 
+To run all the days:
+
+```shell
+$ lein run
+```
+
+This will take some time to complete.
+
+To run a specific day, e.g. day 19:
+
+```shell
 $ lein run 19
+```
 
-How To test
+## Testing
 
+To run all the tests:
+
+```shell
+$ lein test
+```
+
+Not all of the days have tests.
+
+To run a specific test, e.g. tests for day 25:
+
+```shell
 $ lein test :only advent-of-code.core-test/day-25
+```
--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject advent-of-code "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject advent-of-code "1.0.0"
+  :description "Advent of Code 2015"
+  :url "https://github.com/bnomis/advent-of-code-2015"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [
                   [org.clojure/clojure "1.7.0"]
                   [org.clojure/data.json "0.2.6"]
                   [org.clojure/math.combinatorics "0.1.1"]
                   [org.clojure/core.logic "0.8.10"]]
-                  
+
   :main ^:skip-aot advent-of-code.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
